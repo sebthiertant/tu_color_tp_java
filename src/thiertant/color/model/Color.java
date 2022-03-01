28faff24ba4c273b6e9d03c151e14924aa2f65fb
SebThiertant <sebastien.thiertant@gmail.com>
2022-03-01 14:15:02 +0100

    Constructeur à trois paramètres testé

--- src/thiertant/color/model/Color.java
+++ src/thiertant/color/model/Color.java
@@ -2,4 +2,70 @@ package thiertant.color.model;
 
 public class Color {
 
+    private int red;
+    private int green;
+    private int blue;
+    private String hexValue;
+
+    public Color(int red, int green, int blue){
+
+        if (red >= 0 && red < 255) {
+            this.red = red;
+        }
+        else {
+            throw new IllegalArgumentException("Red color is not valid");
+        }
+
+        if (green >= 0 && green < 255){
+            this.green = green;
+        }
+        else {
+            throw new IllegalArgumentException("Green color is not valid");
+        }
+
+        if (blue >= 0 && blue < 255){
+            this.blue = blue;
+        }
+        else {
+            throw new IllegalArgumentException("Blue color is not valid");
+        }
+
+    }
+
+    public Color(String hexadecimalString){
+        this.hexValue = hexadecimalString;
+    }
+
+    public int getRed(){
+        return this.red;
+    }
+
+    public void setRed(int red){
+        this.red = red;
+    }
+
+    public int getGreen(){
+        return this.green;
+    }
+
+    public void setGreen(int green){
+        this.green = green;
+    }
+
+    public int getBlue(){
+        return this.blue;
+    }
+
+    public void setBlue(int blue){
+        this.blue = blue;
+    }
+
+    public String getHexValue(){
+        return this.hexValue;
+    }
+
+    public void setHexValue(String hexValue){
+        this.hexValue = hexValue;
+    }
+
 }
--- test/thiertant/color/model/ColorTest.java
+++ test/thiertant/color/model/ColorTest.java
@@ -1,4 +1,22 @@
 package thiertant.color.model;
 
-public class ColorTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ColorTest {
+
+    private Color color;
+
+    @BeforeEach
+    public void setUp() {
+        this.color = new Color(55, 5,15);
+    }
+
+    @Test
+    public void testConstructorWithThreeArguments() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new Color(256,251,15));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new Color(15,256,15));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new Color(35,251,256));
+    }
 }
